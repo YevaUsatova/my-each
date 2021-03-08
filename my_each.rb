f08589286bdf34690fa0693eaae4417e7bfeb459
YevaUsatova <yeva.usatova@gmail.com>
2021-03-07 22:31:27 -0500

    commit tree 9d113e77f101bf699241cf050ee9de31948fe69d

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,21 @@
-def my_each # put argument(s) here
+
+def my_each(collection) # put argument(s) here
   # code here
+  new_array = []
+  for element in collection
+    new_array.push yield element
+  end
+  collection
+  #i = 0
+  #while i > collection.length
+     #item = collection[i]
+     #yield (collection)
+    #item +=1
+    #end  
+  #collection 
+end
+
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+   i +=1
 end
